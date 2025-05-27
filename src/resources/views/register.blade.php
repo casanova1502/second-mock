2e96ff0da6a242cdf1644cbb93c090a6d24d5c57
ksk@csnv <ptkr.psyot2021@gmail.com>
2025-05-27 16:06:02 +1000

    会員登録記述済み

---
+++ src/resources/views/register.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>coachtech 勤怠管理アプリ</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+</head>
+
+<body>
+    <div class="all-contents">
+        <div class="header-contents">
+            <img src="" alt="">
+            <!-- 画像を入れます -->
+        </div>
+        <div class="main-contents">
+            <h1>会員登録</h1>
+            <!-- <form action=""></form> -->
+                <!-- @csrf -->
+                <label class="label">名前</label>
+                <input type="text" name="name" class="resigter_contents" value="{{ old('name')}}" />      
+                <label class="label">メールアドレス</label>
+                <input type="email" name="email" class="resigter_contents" value="{{ old('email') }}" />
+                <label class="label">パスワード</label>
+                <input type="password" name="password" class="resigter_contents" />
+                <label class="label">確認用パスワード</label>
+                <input type="password" name="password_confirmation" class="resigter_contents" />
+                <button  type="submit" class="button-register">登録する</button>
+            <!-- </form> -->
+            <!-- <a href="/login">ログインはこちら</a> -->
+            <!-- ルート処理をしてから解除 -->
+        </div>    
+    </div>
+</body>
