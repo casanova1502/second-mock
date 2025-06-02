f77462dd21b84324d8567e4b4d679bf20463ff95
ksk@csnv <ptkr.psyot2021@gmail.com>
2025-06-02 18:51:30 +1000

    管理者ログイン画面html

---
+++ src/resources/views/admin/admin_login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>coachtech 勤怠管理アプリ</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+</head>
+
+<body>
+    <div class="all-contents">
+        <div class="header-contents">
+            <img src="storage/logo.svg" alt="ロゴ">
+        </div>
+        <div class="main-contents">
+            <h1>管理者ログイン</h1>
+            <!-- <form class="form" action="/login" method="post"> -->
+                <!-- @csrf -->
+                <label class="label">メールアドレス</label>
+                <input type="email" name="email" class="login_contents" value="{{ old('email') }}" />
+                <label class="label">パスワード</label>
+                <input type="password" name="password" class="login_contents" />
+                <button type="submit" class="button-login">管理者ログインする</button>
+            <!-- </form> -->
+            <!-- <a href="/register">会員登録はこちら</a> -->
+            <!-- ルート処理をしてから解除 -->
+        </div>    
+    </div>
+</body>
