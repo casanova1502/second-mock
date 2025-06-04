9f71fb21d2b0d34c37b5dc732f798ebfb03b1997
ksk@csnv <ptkr.psyot2021@gmail.com>
2025-06-04 13:35:33 +1000

    login css 実装済み

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -8,6 +8,7 @@
   <title>coachtech 勤怠管理アプリ</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
   <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
 </head>
 
 <body>
@@ -19,13 +20,17 @@
             <h1>ログイン</h1>
             <!-- <form class="form" action="/login" method="post"> -->
                 <!-- @csrf -->
-                <label class="label">メールアドレス</label>
-                <input type="email" name="email" class="login_contents" value="{{ old('email') }}" />
-                <label class="label">パスワード</label>
-                <input type="password" name="password" class="login_contents" />
+                <div class="form-group">
+                    <label class="label">メールアドレス</label>
+                    <input type="email" name="email" class="login_contents" value="{{ old('email') }}" />
+                </div>    
+                <div class="form-group">
+                    <label class="label">パスワード</label>
+                    <input type="password" name="password" class="login_contents" />
+                </div>
                 <button type="submit" class="button-login">ログインする</button>
             <!-- </form> -->
-            <!-- <a href="/register">会員登録はこちら</a> -->
+            <a href="/register" class="href">会員登録はこちら</a>
             <!-- ルート処理をしてから解除 -->
         </div>    
     </div>
